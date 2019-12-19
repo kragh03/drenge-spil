99141c01bd3e0354fc906df3e29cb9ac79c2ef5d
kragh <kasperkragheliasen@gmail.com>
2019-12-19 14:52:51 +0100

    git push

--- rumskib 2/sketch.js
+++ rumskib 2/sketch.js
@@ -102,5 +102,15 @@ function Ship(){
    
  }
 
+ function Asteroid(){
+  this.pos = createVector(random(width), random(height))
+  this.r = 50;
+  this.render = function() {
+
+   translate(this.pos.x, this.pos.y);
+   ellipse(0, 0, this.r*2);
+  }
+}
+
 
 
