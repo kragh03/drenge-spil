9a6f204a6def960ff40e33c41dc5fd504feafceb
kragh <kasperkragheliasen@gmail.com>
2020-01-09 14:27:41 +0100

    fix

---
+++ rumskib 2/laser.js
@@ -0,0 +1,15 @@
+function Laser(spos, angle) {
+    this.pos = createVector(spos.x, spos.y);
+    this.vel = p5.Vector.fromAngle(angle);
+    
+    this.update = function () {
+        this.pos.add(this.vel);
+    }
+    this.render = function () {
+        push();
+        stroke(255);
+        strokeWeight(4);
+        point(this.pos.x, this.pos.y);
+        pop();
+    }
+}
--- rumskib 2/sketch.js
+++ rumskib 2/sketch.js
@@ -1,12 +1,13 @@
 var ship;
 var asteroids = [];
+var lasers = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   ship = new Ship();
   for (var i = 0; i < 5; i++) {
-  asteroids.push(new Asteroid());
-}
+    asteroids.push(new Asteroid());
+  }
 }
 
 function draw() {
@@ -16,99 +17,106 @@ function draw() {
   ship.update();
   ship.edges();
 
-  for (var i =0; i < asteroids.length; i++){
+  for (var i = 0; i < asteroids.length; i++) {
     asteroids[i].render();
     asteroids[i].update();
     asteroids[i].edges();
   }
+  for (var i = 0; i < lasers.length; i++) {
+    lasers[i].render();
+    lasers[i].update();
 
+  }
 }
-function keyReleased(){ 
-ship.setRotation(0);
-ship.boosting(false);
+function keyReleased() {
+  ship.setRotation(0);
+  ship.boosting(false);
 }
 
 
 // drejning af skip
-function keyPressed(){
-if(keyCode == RIGHT_ARROW) {
-ship.setRotation(0.1);
-} else if(keyCode == LEFT_ARROW){
-ship.setRotation(-0.1);
-} else if (keyCode == UP_ARROW){
-ship.boosting(true);
+function keyPressed() {
+  if (key == ' ') {
+    lasers.push(new Laser(ship.pos, ship.heading));
+
+  } else if (keyCode == RIGHT_ARROW) {
+    ship.setRotation(0.1);
+  } else if (keyCode == LEFT_ARROW) {
+    ship.setRotation(-0.1);
+  } else if (keyCode == UP_ARROW) {
+    ship.boosting(true);
+  }
+
 }
-  
-}
 
 
 
-function Ship(){
-  this.pos = createVector(width/2, height/2);
-   //r er størrelse på skibet 
-   this.r = 20;
-   this.heading= 0;
-   this.rotation = 0;
-   this.vel =createVector(0,0);
-   this.isBoosting = false;
-   
-   
-   this.boosting = function(b) {
-   this.isBoosting = b;
-   }
-   
-   this.update = function() {
-     if(this.isBoosting){
-     this.boost();
-     }
-     this.pos.add(this.vel);
-     //stopper skibet igen så den ikke forsætter
-   this.vel.mult(0.99);  
-   }
-   // kald den måske fartstribe
-   this.boost = function(){
-   var force = p5.Vector.fromAngle(this.heading); 
- force.mult(0.1);
- this.vel.add(force);
-   }
-   
-   
-   this.render = function(){ 
-     push();
-       translate(this.pos.x, this.pos.y);
-     rotate(this.heading + PI / 2);
+function Ship() {
+  this.pos = createVector(width / 2, height / 2);
+  //r er størrelse på skibet 
+  this.r = 20;
+  this.heading = 0;
+  this.rotation = 0;
+  this.vel = createVector(0, 0);
+  this.isBoosting = false;
+
+
+  this.boosting = function (b) {
+    this.isBoosting = b;
+  }
+
+  this.update = function () {
+    if (this.isBoosting) {
+      this.boost();
+    }
+    this.pos.add(this.vel);
+    //stopper skibet igen så den ikke forsætter
+    this.vel.mult(0.99);
+  }
+  // kald den måske fartstribe
+  this.boost = function () {
+    var force = p5.Vector.fromAngle(this.heading);
+    force.mult(0.1);
+    this.vel.add(force);
+  }
+
+
+  this.render = function () {
+    push();
+    translate(this.pos.x, this.pos.y);
+    rotate(this.heading + PI / 2);
     //tømme skibet og gøre stregen hvid
-     noFill();
-     stroke(255);
-       triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
-      pop();
-      }
-   
-   this.edges =function(){
-   if (this.pos.x > width + this.r){
-   this.pos.x = -this.r;
-   } else if (this.pos.x< -this.r){
-   this.pos.x =width + this.r;
-   }
-       if (this.pos.y > height + this.r){
-   this.pos.y = -this.r;
-   } else if (this.pos.y< -this.r){
-   this.pos.y =height + this.r;
-   }
-   }
-     
-     
-   this.setRotation = function(a) {
-   this.rotation = a;
-   }
-   
-   this.turn = function(angle){
-   this.heading += this.rotation;
-   }
-   
- }
+    noFill();
+    stroke(255);
+    triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
+    pop();
+  }
 
- 
+  this.edges = function () {
+    if (this.pos.x > width + this.r) {
+      this.pos.x = -this.r;
+    } else if (this.pos.x < -this.r) {
+      this.pos.x = width + this.r;
+    }
+    if (this.pos.y > height + this.r) {
+      this.pos.y = -this.r;
+    } else if (this.pos.y < -this.r) {
+      this.pos.y = height + this.r;
+    }
+  }
+
+
+  this.setRotation = function (a) {
+    this.rotation = a;
+  }
+
+  this.turn = function (angle) {
+    this.heading += this.rotation;
+  }
+
+}
+
+
 
 
 
