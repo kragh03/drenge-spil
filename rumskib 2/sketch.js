5800157a6deca35669e150c008e9861242053ea5
kragh <kasperkragheliasen@gmail.com>
2020-01-16 14:24:51 +0100

    fix x

--- rumskib 2/sketch.js
+++ rumskib 2/sketch.js
@@ -19,15 +19,18 @@ function draw() {
     asteroids[i].update();
     asteroids[i].edges();
   }
-  for (var i = 0; i < lasers.length; i++) {
+  for (var i = lasers.length-1; i >=0 ; i--) {
     lasers[i].render();
     lasers[i].update();
     for (var j = asteroids.length - 1; j >= 0; j--) {
       if (lasers[i].hits(asteroids[j])) {
         var newAsteroids = asteroids[j].breakup();
         console.log(newAsteroids);
-        // asteroids.push(newAsteroids);
+        asteroids = asteroids.concat(newAsteroids);
+        console.log(asteroids);
         asteroids.splice(j, 1);
+        lasers.splice(i, 1);
+        break;
       }
     }
   }
