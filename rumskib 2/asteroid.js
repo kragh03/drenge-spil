3ff6f364fa68e30ba7bac698858dd8a5be3522a4
yde6 <micyde@live.dk>
2020-01-16 15:01:06 +0100

    DetVirker

--- rumskib 2/asteroid.js
+++ rumskib 2/asteroid.js
@@ -1,16 +1,21 @@
 
-function Asteroid(pos) {
+function Asteroid(pos, r) {
     if (pos) {
-        this.pos = pos.copy;
+        this.pos = pos.copy();
     } else {
         this.pos = createVector(random(width), random(height))
     }
+    if (r) {
+        this.r = r * 0.5;
+    } else {
+        this.r = random(15, 50);
+    }
+
     this.vel = p5.Vector.random2D();
-    this.r = random(15, 50);
     this.total = floor(random(5, 15));
     this.offset = [];
     for (let i = 0; i < this.total; i++) {
-        this.offset[i] = random(-5, 15);
+        this.offset[i] = random(-this.r * 0.5, this.r * 0.5);
     }
     this.update = function () {
         this.pos.add(this.vel);
@@ -37,8 +42,8 @@ function Asteroid(pos) {
     }
     this.breakup = function () {
         var newA = [];
-        newA[0] = new Asteroid(this.pos);
-        newA[1] = new Asteroid(this.pos);
+        newA[0] = new Asteroid(this.pos, this.r);
+        newA[1] = new Asteroid(this.pos, this.r);
         return newA;
     }
     this.edges = function () {
--- rumskib 2/sketch.js
+++ rumskib 2/sketch.js
@@ -19,15 +19,18 @@ function draw() {
     asteroids[i].update();
     asteroids[i].edges();
   }
-  for (var i = 0; i < lasers.length; i++) {
+  for (var i = lasers.length - 1; i >= 0; i--) {
     lasers[i].render();
     lasers[i].update();
     for (var j = asteroids.length - 1; j >= 0; j--) {
       if (lasers[i].hits(asteroids[j])) {
-        var newAsteroids = asteroids[j].breakup();
-        console.log(newAsteroids);
-        //asteroids.push(newAsteroids);
+        if (asteroids[j].r > 10) {
+          var newAsteroids = asteroids[j].breakup();
+          asteroids = asteroids.concat(newAsteroids);
+        }
         asteroids.splice(j, 1);
+        lasers.splice(i, 1);
+        break;
       }
     }
   }
